e7e6520e03613b2399f626084115ac6ee7565a6c
ariannagreco3393 <a.greco3393@gmail.com>
2022-05-02 22:02:51 +0200

    initial commit

---
+++ vue-cli-first/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MYtitle/>
+  </div>
+</template>
+
+<script>
+import MYtitle from './components/title.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MYtitle
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli-first/src/components/title.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="hi">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MYtitle',
+}
+</script>
+
+<style scoped>
+    .hi{
+        color: aquamarine;
+    }
+</style>
